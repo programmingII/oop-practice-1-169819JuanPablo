32d774a237330f08d92704dc9dbccc022f7e9328
169819JuanPablo <46726315+169819JuanPablo@users.noreply.github.com>
2019-02-05 13:12:06 -0700

    Add files via upload

---
+++ Ejercicio1_helloworld.java
@@ -0,0 +1,11 @@
+/**
+* Juan Pablo Martinez
+* 02/05/2019
+* 12:30
+*/
+
+class helloworld{
+	public static void main(String [] args){
+		System.out.println("Hello  World, this is my first program!");
+}
+}
