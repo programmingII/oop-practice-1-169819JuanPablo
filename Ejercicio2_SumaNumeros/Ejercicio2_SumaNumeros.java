40196e1158852d11db2ac8e8c899adc027bae0ef
169819JuanPablo <46726315+169819JuanPablo@users.noreply.github.com>
2019-02-05 13:28:49 -0700

    Add files via upload

---
+++ Ejercicio2_SumaNumeros/Ejercicio2_SumaNumeros.java
@@ -0,0 +1,19 @@
+/**
+* Juan Pablo Martinez
+* 05/02/2019
+* 12:40 pm
+*/
+import java.util.Scanner;
+
+class Ejercicio2{
+	public static void main (String[] args){
+		int var1,var2;
+		Scanner t=new Scanner(System.in);
+		System.out.print("Introduzca num 1: \n");
+		var1= t.nextInt();
+		System.out.print("Introduzca num 2: \n");
+		var2= t.nextInt();
+		System.out.println(var1 + "+" + var2 + "=" + (var1+var2));
+}		
+
+}
